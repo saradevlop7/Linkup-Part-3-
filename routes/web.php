5e373ca30471498d438c57800aa333557b6ed73a
sara charafi <elkbircharafi8@gmail.com>
2026-07-09 16:54:36 +0100

    Complete likes feature and update feed

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 
+
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 // Feed
