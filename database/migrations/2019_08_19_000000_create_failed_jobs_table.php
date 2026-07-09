2f043fe010e642db6f5934318939c5651175b475
sara charafi <elkbircharafi8@gmail.com>
2026-07-09 12:06:42 +0100

    Create Like model with relationships

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -11,7 +11,6 @@ return new class extends Migration
     {
         Schema::create('failed_jobs', function (Blueprint $table) {
             $table->id();
-            
             $table->string('uuid')->unique();
             $table->text('connection');
             $table->text('queue');
